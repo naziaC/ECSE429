9d52fc9814ae7d7ea6fe68eee01151b4b048255d
naziaC <nazia.chowdhury@mail.mcgill.ca>
2024-10-06 15:38:28 -0400

    Setup gradle for testing

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiTest {
+
+    @Test
+    void testSomeFunctionality() {
+        // Example test
+        assertEquals(2, 1 + 1);
+    }
+}
